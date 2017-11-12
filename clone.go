c1bbc16ad5bd7d1be96e9bf3c0be4cb7db199421
Michael Jackson <majgis@gmail.com>
2017-11-12 12:15:52 -0700

    Add simple handling of first argument

--- clone.go
+++ clone.go
@@ -1,7 +1,17 @@
 package main
 
+import "os"
 import "fmt"
 
+func usage() {
+	fmt.Println("No bueno")
+}
+
 func main() {
-	fmt.Printf("hello, world\n")
+	if len(os.Args) == 1 {
+		usage()
+		os.Exit(0)
+	}
+	repo := os.Args[1]
+	fmt.Println(repo)
 }

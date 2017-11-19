ea8f9ec0a52d2cecf138bb37a186bb977ad15698
Michael Jackson <mjackson@overstock.com>
2017-11-19 10:01:35 -0700

    Handle clone command

--- gittk.go
+++ gittk.go
@@ -3,19 +3,42 @@ package main
 import "os"
 import "fmt"
 
-func usage() {
+func printUsage() {
 	fmt.Println(`
-Usage:
+Commands:
 
-clone <git URI>      
+clone - Clone git repositories into consistent tree structure
+        $ gittk clone <git URI>      
 `)
 }
 
 func main() {
+
+	// Print usage if no command was given
 	if len(os.Args) == 1 {
-		usage()
+		printUsage()
 		os.Exit(0)
 	}
-	repo := os.Args[1]
-	fmt.Println(repo)
+
+	command := os.Args[1]
+
+	// Execute the given command
+	switch command {
+	case "clone":
+		if len(os.Args) < 3 {
+			fmt.Println("You must supply git URI to the clone command.")
+			printUsage()
+			os.Exit(1)
+		}
+		repo := os.Args[2]
+		clone(repo)
+	default:
+		fmt.Println("The given command is unknown.")
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func clone(repo string) {
+	fmt.Printf("TODO: clone %v\n", repo)
 }

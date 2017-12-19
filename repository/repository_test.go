a8151bec5f49c85ac7eb1716d7ab0389333e76c7
Michael Jackson <mjackson@overstock.com>
2017-12-18 22:54:27 -0700

    Add a test for repository.GetDir

--- repository/repository_test.go
+++ repository/repository_test.go
@@ -1,16 +1,35 @@
 package repository
 
 import "testing"
+import "os/user"
+import "path/filepath"
 
-func TestHelloWorld(t *testing.T) {
+func TestGetSubDir(t *testing.T) {
 	actual, err := GetSubDir("https://github.com/majgis/gittk.git")
 	if err != nil {
 		t.Fatalf("Error when getting directory: %v", err)
 	}
 
-	expected := "github.com/majgis/gittk"
+	expected := filepath.Join("github.com", "majgis", "gittk")
 
 	if actual != expected {
-		t.Fatalf("%v does not equal %v", actual, expected)
+		t.Fatalf("'%v' does not equal '%v'", actual, expected)
+	}
+}
+
+func TestGetDir(t *testing.T) {
+	actual, err := GetDir("https://github.com/majgis/gittk.git")
+	if err != nil {
+		t.Fatalf("Error when getting directory: %v", err)
+	}
+
+	user, usrErr := user.Current()
+	if usrErr != nil {
+		t.Fatalf("Error retrieving user: %v", usrErr)
+	}
+
+	expected := filepath.Join(user.HomeDir, "projects", "github.com", "majgis", "gittk")
+	if actual != expected {
+		t.Fatalf("'%v' does not equal '%v'", actual, expected)
 	}
 }
